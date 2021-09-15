20bb672b105aa6a73d0338a1803553a3036ec9ec
meljith-lab <83987293+meljith-lab@users.noreply.github.com>
2021-09-15 15:41:01 +0530

    Update ARecon.sh

--- ARecon.sh
+++ ARecon.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+
+
+echo Please input the domain you would like to scan:
+read domain
+
+subdomainEnum(){
+	mkdir -p $domain $domain/subs $domain/recon
+	subfinder -d $domain -o $domain/subs/SubfinderResults.txt
+	amass enum -d  $domain -o $domain/subs/amassResults.txt
+	cat $domain/subs/*.txt > $domain/subs/allSubsFound.txt
+}
+subdomainEnum
+
+subdomainValidation(){
+	cat $domain/subs/allSubsFound.txt |httprobe > $domain/subs/Httprobe.txt
+}
+subdomainValidation
+
+subdomainPortScanner(){
+        cat $domain/subs/allSubsFound.txt | naabu -top-ports 1000 > $domain/subs/Sub_Port.txt
+}
+subdomainPortScanner
 
